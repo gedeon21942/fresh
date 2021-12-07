80b1e3dbb584a1dee51c0fee01d8ebc473ce18e0
gedeon21942 <gedeon21942@gmail.com>
2021-12-07 08:35:15 -0500

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt install -y update
+sudo apt -y update
 sudo apt install -y openssh-server
-#sudo apt -y xrdp
-#sudo apt -y tightvncserver
+sudo apt install  -y xrdp
+sudo apt install -y tightvncserver
 sudo apt install -y samba
 sudo nano /etc/samba/smb.conf
