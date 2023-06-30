8451c21514931240304a31dd9d01fda97576c287
gedeon21942 <gedeon21942@gmail.com>
2023-06-29 22:56:21 -0400

    Delete fresh_install.sh

--- fresh_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt -y update;
-sudo apt install -y openssh-server;
-sudo apt install  -y xrdp;
-sudo apt install -y tightvncserver;
-sudo apt install -y samba;
-sudo nano /etc/samba/smb.conf;
