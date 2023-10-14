d4735b9d7f0eba416e76d1d067d4a16b8d703728
gedeon21942 <gedeon21942@gmail.com>
2023-10-14 13:43:06 -0400

    Add files via upload

---
+++ 2-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo rm /home/nortron/dotfiles/.bashrc
+sudo cp /mnt/share/linux/bashfile/2.0/.bashrc /home/nortron/dotfiles
+source ~/.bashrc
+sudo cp /mnt/share/linux/bashfile/Demo.ckb /opt
+sudo cp /mnt/share/linux/bashfile/install.sh /opt
