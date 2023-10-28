f9d8313cf52d98841c40a0f44ad69ea56b7fd6a2
gedeon21942 <gedeon21942@gmail.com>
2023-10-28 12:48:39 -0400

    Add files via upload

--- share.sh
+++ share.sh
@@ -3,6 +3,4 @@ sudo mkdir /mnt/share
 sudo mkdir /mnt/share/linux
 sudo cp linux.sh /opt
 sudo cp share.sh /opt
-sudo cp exuc.sh /opt
-sudo cp sshd.sh /opt
-sudo rm -R fresh/
+sudo cp 2-install.sh /opt
